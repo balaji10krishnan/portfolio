5901e093bf44e024e0859a008d7d5c671cd0f2cd
balaji10krishnan <balaji10krishnan@gmail.com>
2024-09-20 08:48:27 +0530

    add the suspense loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import logo from "./logo.svg";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Header from "./component/features/Header/Header";
-import Home from "./component/sections/Home/Home";
-import About from "./component/sections/About/About";
-import Skills from "./component/sections/Skills/Skills";
-import Projects from "./component/sections/Projects/Projects";
-import Contact from "./component/sections/Contact/Contact";
+import Loading from "./component/features/Loading/Loading";
+
+// Lazy load your sections
+const Home = lazy(() => import("./component/sections/Home/Home"));
+const About = lazy(() => import("./component/sections/About/About"));
+const Skills = lazy(() => import("./component/sections/Skills/Skills"));
+const Projects = lazy(() => import("./component/sections/Projects/Projects"));
+const Contact = lazy(() => import("./component/sections/Contact/Contact"));
 
 function App() {
   return (
     <div data-theme="">
       <Header />
-      <div id="home">
-        <Home />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
+      <Suspense fallback={<Loading />}>
+        <div id="home">
+          <Home />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="skills">
+          <Skills />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+      </Suspense>
     </div>
   );
 }
---
+++ src/component/features/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import classes from "./Loading.module.css";
+const Loading = () => {
+  return (
+    <>
+      <div
+        className={classes.loading}
+        data-loading-text="Balaji_Krishnan"
+      ></div>
+    </>
+  );
+};
+export default Loading;
