860bf2c364c92561fa21fb390607faae38b8c0b4
balaji10krishnan <balaji10krishnan@gmail.com>
2024-10-06 17:23:54 +0530

    project and title change

--- src/component/sections/Projects/Projects.jsx
+++ src/component/sections/Projects/Projects.jsx
@@ -1,20 +1,25 @@
 import ProjectCard from "../../ui/ProjectCard/ProjectCard";
 import Title from "../../ui/Title/Tiltle";
 import classes from "./Projects.module.css";
-import img from "../../../assets/img/project-1.png";
+import sky from "../../../assets/img/skyscanner.png";
+import expense_tracker from "../../../assets/img/expense_tracker.png";
 const Projects = () => {
   return (
     <>
       <Title title1="Projects" title2="Browse My Recent" />
       <section className={classes["project-section"]}>
         <ProjectCard
-          img={img}
+          img={expense_tracker}
+          github="https://github.com/balaji10krishnan/expense_tracker"
+          livedemo="https://expense-tracker-ce79e.web.app/"
+          title="Expense Tracker"
+        />
+        <ProjectCard
+          img={sky}
           github="https://github.com/balaji10krishnan/weather"
           livedemo="https://balaji10krishnan.github.io/weather/"
           title="Sky Scanner"
         />
-        <ProjectCard img={img} />
-        <ProjectCard img={img} />
       </section>
     </>
   );
